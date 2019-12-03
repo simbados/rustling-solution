f4b7ee063bdcb2568fa5c567443c8b6fc8ffdcc1
Simon Schmoll <simon.schmoll@protonmail.com>
2019-12-03 11:09:55 +0100

    Add solution for functions

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -4,19 +4,20 @@
 // This store is having a sale where if the price is an even number, you get
 // 10 (money unit) off, but if it's an odd number, it's 3 (money unit) less.
 
-// I AM NOT DONE
-
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
+    println!("Orginal value is {}", original_price);
 }
 
-fn sale_price(price: i32) -> {
-    if is_even(price) {
-        price - 10
+fn sale_price(mut price: i32) -> i32 {
+    let mut sales_price = price;
+    if is_even(sales_price) {
+        sales_price -= 10
     } else {
-        price - 3
+        sales_price -= 3
     }
+    return sales_price
 }
 
 fn is_even(num: i32) -> bool {
