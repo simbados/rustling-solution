d36354004b7fa4df143fbcff21dc8848c9720e46
Simon Schmoll <simon.schmoll@protonmail.com>
2019-12-03 13:11:13 +0100

    Add solution for tests

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,18 +4,16 @@
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` for hints :)
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use is_even;
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(12));
     }
 }
